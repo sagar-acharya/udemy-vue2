522e2e37c0ee6eb27f71cd58a93d45804e0447ed
Sagar Acharya <sagar_acharya@live.in>
2017-03-13 20:55:22 +0530

    components communication

---
+++ exercise-07/src/components/Server/Servers.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">
+            <app-server v-for="server in servers" :server="server"></app-server>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import Server from './Server.vue';
+    export default {
+        data: function() {
+            return {
+                servers: [
+                    { id: 1, name: 'Normal' },
+                    { id: 2, name: 'Critical' },
+                    { id: 3, name: 'Unknown' },
+                    { id: 4, name: 'Normal' }
+                ]
+            }
+        },
+        components: {
+            'app-server': Server
+        }
+    }
+</script>
+
+<style>
+
+</style>
